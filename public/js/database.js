6e639feaabc6d9fadf2f03e92bf4306185bda8e3
kou256 <25428510+kou256@users.noreply.github.com>
2022-09-30 16:56:54 +0900

    ✨ 重複した名前を登録しない処理
    登録時に同じ名前のものは登録しないようにした．

--- public/js/database.js
+++ public/js/database.js
@@ -10,8 +10,30 @@ async function getChildKeys(path) {
   return keys
 }
 
-function updateData(path, data) {
+async function updateData(path, data) {
+  const keys = Object.keys(data)
+  // 今のところkeysが複数になることは想定していない（forEachは1回しか実行されない想定）
+  const exists = await existsData(path, keys[0])
+  if (!exists) {
+    const ref = firebase.database().ref(path)
+
+    return ref.update(data)
+
+  } else {
+    alert('すでに存在しています')
+
+    return undefined
+  }
+}
+
+async function existsData(path, key) {
+  console.log(path)
   const ref = firebase.database().ref(path)
 
-  return ref.update(data)
+  const result = await ref.get()
+  if (result.child(key).exists()) {
+    return true
+  } else {
+    return false
+  }
 }
