08d29f2cda06534b55d57147045aa653ed81daaf
oudon2430 <kenshi2430@gmail.com>
2022-10-03 18:12:15 +0900

    作業ページ機能統合(登録・削除・動画表示・PDF表示)

---
+++ public/auto_video.js
@@ -0,0 +1,50 @@
+function judgementVideo(fileName){
+    // fileName = fileName.substring(fileName.lastIndexOf('.'))
+    if (fileName.toUpperCase().match(/\.(mp4)$/i)) {return true}
+    if (fileName.toUpperCase().match(/\.(avi)$/i)) {return true}
+    if (fileName.toUpperCase().match(/\.(fiv)$/i)) {return true}
+    if (fileName.toUpperCase().match(/\.(mov)$/i)) {return true}
+    if (fileName.toUpperCase().match(/\.(wmv)$/i)) {return true}
+    return false
+}
+
+function createVideo(path){
+    const storage = firebase.storage();
+    const ref = storage.ref(path); 
+
+    ref.getDownloadURL().then((url) => {          
+        var video = document.createElement("video");
+        video.src=url; // 動的に生成した動画のURL
+        video.setAttribute("controls","");
+        document.body.appendChild(video);
+        document.body.appendChild(document.createElement("br"));
+    });
+}
+
+var projectname = "pj1"
+var workname = "work1"
+
+window.addEventListener('load', function(){  
+
+    const storage = firebase.storage();
+    var ref_path = projectname + "/" + workname
+    console.log(ref_path)
+    const storageRef = storage.ref(); 
+    var listRef = storageRef.child(ref_path);
+
+    listRef.listAll().then((res) => {
+        res.prefixes.forEach((folderRef) => {
+        });
+        res.items.forEach((itemRef) => {
+            console.log(itemRef)
+            console.log(itemRef.fullPath)
+            if (judgementVideo(itemRef.fullPath) == true){
+                console.log('true')
+                createVideo(itemRef.fullPath);
+            }
+            else{console.log('false')}
+        });
+    }).catch((error) => {
+    console.error(error);
+    });
+});
---
+++ public/delete.js
@@ -0,0 +1,77 @@
+function deleteFileCheck(fileName){
+    var checkBox = document.createElement('input');
+    checkBox.setAttribute('type','checkbox');
+    checkBox.setAttribute('name','chk');
+    checkBox.setAttribute('value',fileName);
+    var label = document.createElement('label');
+    label.appendChild(checkBox);
+    var text_node = document.createTextNode(fileName);
+    label.appendChild(text_node)
+
+    document.getElementById('contentsList').appendChild(label);
+    document.getElementById('contentsList').appendChild(document.createElement("br"));
+}
+
+
+var projectname = "pj1"
+var workname = "work1"
+
+window.addEventListener('load', function(){  
+
+    const storage = firebase.storage();
+    var ref_path = projectname + "/" + workname
+    console.log(ref_path)
+    const storageRef = storage.ref(); 
+    var listRef = storageRef.child(ref_path);
+
+    listRef.listAll().then((res) => {
+        res.prefixes.forEach((folderRef) => {
+        });
+        res.items.forEach((itemRef) => {
+            console.log(itemRef)
+            console.log(itemRef.name)
+            deleteFileCheck(itemRef.name)
+        });
+    }).catch((error) => {
+    console.error(error);
+    });
+});
+
+function OnDeleteClick() {
+    const storage = firebase.storage();
+    var ref_path = projectname + "/" + workname
+    const storageRef = storage.ref(); 
+
+    const arr = [];
+    let chklist = document.getElementById('contentsList').chk;
+
+    console.log(chklist);
+    if (chklist.length === undefined) {
+        if(chklist.checked){
+            var deleteRef = storageRef.child(ref_path + '/' + chklist.value);
+                console.log(deleteRef);
+                deleteRef.delete().then(() => {
+                    console.log('Deleted a file!');
+                    alert("次のファイルを削除しました\n" + chklist.value);
+                }).catch((error) => {
+                    console.error(error)
+                });
+        }
+    }
+    else {
+        for (let i = 0; i < chklist.length; i++) {
+            if (chklist[i].checked) {
+                var deleteRef = storageRef.child(ref_path + '/' + chklist[i].value);
+                console.log(deleteRef);
+                deleteRef.delete().then(() => {
+                    console.log('Deleted a file!');
+                    alert("次のファイルを削除しました\n" + chklist[i].value);
+                }).catch((error) => {
+                    console.error(error)
+                });
+                // arr.push(chklist[i].value);
+            }
+        }
+    }
+    // console.log(arr);
+}
