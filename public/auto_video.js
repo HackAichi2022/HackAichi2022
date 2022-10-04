4cc41b97f1e3b26339c25d6a2064559afccafd36
oudon2430 <kenshi2430@gmail.com>
2022-10-04 16:02:37 +0900

    マージ

--- public/auto_video.js
+++ public/auto_video.js
@@ -1,23 +1,27 @@
-function judgementVideo(fileName){
+function judgementVideo(fileName) {
     // fileName = fileName.substring(fileName.lastIndexOf('.'))
-    if (fileName.toUpperCase().match(/\.(mp4)$/i)) {return true}
-    if (fileName.toUpperCase().match(/\.(avi)$/i)) {return true}
-    if (fileName.toUpperCase().match(/\.(fiv)$/i)) {return true}
-    if (fileName.toUpperCase().match(/\.(mov)$/i)) {return true}
-    if (fileName.toUpperCase().match(/\.(wmv)$/i)) {return true}
+    if (fileName.toUpperCase().match(/\.(mp4)$/i)) { return true }
+    if (fileName.toUpperCase().match(/\.(avi)$/i)) { return true }
+    if (fileName.toUpperCase().match(/\.(fiv)$/i)) { return true }
+    if (fileName.toUpperCase().match(/\.(mov)$/i)) { return true }
+    if (fileName.toUpperCase().match(/\.(wmv)$/i)) { return true }
     return false
 }
 
-function createVideo(path){
+function createVideo(path, i) {
     const storage = firebase.storage();
-    const ref = storage.ref(path); 
+    const ref = storage.ref(path);
 
-    ref.getDownloadURL().then((url) => {          
+    ref.getDownloadURL().then((url) => {
         var video = document.createElement("video");
-        video.src=url; // 動的に生成した動画のURL
-        video.setAttribute("controls","");
+        video.src = url; // 動的に生成した動画のURL
+        video.setAttribute("id", "video" + i);
+        video.setAttribute("class", "video" + i);
+        video.setAttribute("controls", "");
+        console.log(video.id)
         document.body.appendChild(video);
         document.body.appendChild(document.createElement("br"));
+        scroll(video.id);
     });
 }
 
@@ -29,8 +33,9 @@ window.addEventListener('load', function(){
     const storage = firebase.storage();
     var ref_path = projectname + "/" + workname
     console.log(ref_path)
-    const storageRef = storage.ref(); 
+    const storageRef = storage.ref();
     var listRef = storageRef.child(ref_path);
+    var i = 1;
 
     listRef.listAll().then((res) => {
         res.prefixes.forEach((folderRef) => {
@@ -38,13 +43,14 @@ window.addEventListener('load', function(){
         res.items.forEach((itemRef) => {
             console.log(itemRef)
             console.log(itemRef.fullPath)
-            if (judgementVideo(itemRef.fullPath) == true){
+            if (judgementVideo(itemRef.fullPath) == true) {
                 console.log('true')
-                createVideo(itemRef.fullPath);
+                createVideo(itemRef.fullPath, i);
+                i += 1;
             }
-            else{console.log('false')}
+            else { console.log('false') }
         });
     }).catch((error) => {
-    console.error(error);
+        console.error(error);
     });
 });
