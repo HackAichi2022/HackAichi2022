f3b8e36c21e6d5c2c10647bd9d9ec0868c665d4c
kou256 <25428510+kou256@users.noreply.github.com>
2022-10-06 14:00:06 +0900

    💄 タグをバッジとして表示
    トップページにおけるプロジェクトのタグをBootstrapのバッジとして表示するようにした．

--- public/get_data.js
+++ public/get_data.js
@@ -22,13 +22,32 @@ async function getChildKeys(path) {
     const projectRef = firebase.database().ref(`${path}/${key}/tag`)
     projectRef.get().then((snapshot) => {
       tag_l = snapshot.val()
-      let key1 = tag_l[0]
+
+      let htmlText =
+        `<a href="#" id="${key}" onclick="location.href='http://localhost:50000/project.html?projectName=${key}'" data-tag="${snapshot.val()}">
+          <font size="6" color="red">${key}</font>
+          <br><br>
+        `
+
+      if (Array.isArray(tag_l) && tag_l.length > 0) {
+        for (let i = 0; i < tag_l.length; i++) {
+          // 現状表示するタグは最大3つにしている
+          if (i >= 3) {
+            break;
+          }
+          htmlText +=
+            `<span class="badge rounded-pill bg-secondary">
+              <font size="2">${tag_l[i]}</font>
+            </span>`
+        }
+      }
+
+      // 閉じタグを追加する
+      htmlText += `</a><p><br></p>`
       workListElem.insertAdjacentHTML(
         'beforeend',
-        `<a href="#" id="${key}" onclick="location.href='http://localhost:50000/project.html?projectName=${key}'" data-tag="${snapshot.val()}"><font size="6" color="red">${key}</font><br><br><font size="2">${key1}</font></a>
-      <p><br></p>`
+        htmlText
       )
-
     })
   })
 
@@ -69,7 +88,7 @@ document.getElementById("make_project_button").onclick = async function () {
     let ClCu = document.getElementById('Client_Customer').value;
     //let tag_arr = {}
     //tag_arr.tag = tag_list;
-    const data = { [Project_name]: { tag: tag_list , Summary_inp: Sum, Participant_inp: Par, ClCu_inp: ClCu } }
+    const data = { [Project_name]: { tag: tag_list, Summary_inp: Sum, Participant_inp: Par, ClCu_inp: ClCu } }
     ref.update(data);
     window.location.reload();
   }
@@ -77,7 +96,7 @@ document.getElementById("make_project_button").onclick = async function () {
 
 
 function addForm() {
-  if (num_tag < make_max_tag){
+  if (num_tag < make_max_tag) {
     var input_data = document.createElement('input');
     input_data.type = 'text';
     input_data.id = 'inputform_' + num_tag;
@@ -85,7 +104,7 @@ function addForm() {
     var parent = document.getElementById('form_area');
     parent.appendChild(input_data);
     num_tag++;
-  } else{
+  } else {
     alert('this project name is existing!')
   }
 }
@@ -93,10 +112,10 @@ function addForm() {
 document.getElementById('search-button').addEventListener('click', () => {
   const tag = document.getElementById('search-tag').value
   const projectList = document.getElementById('make_pro')
-  projectList.querySelectorAll(`a:not([data-tag*=${tag}])`).forEach((elem) => {
+  projectList.querySelectorAll(`a: not([data - tag*=${tag}])`).forEach((elem) => {
     elem.style.display = 'none'
   })
-  projectList.querySelectorAll(`a[data-tag*=${tag}]`).forEach((elem) => {
+  projectList.querySelectorAll(`a[data - tag*= ${tag}]`).forEach((elem) => {
     elem.style.display = ''
   })
 })
